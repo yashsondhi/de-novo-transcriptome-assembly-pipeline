ce4bc74e5d27a0c1b8df763de6271c3232f239b4
Yash Sondhi <yashsondhi@gmail.com>
2021-12-30 11:08:55 -0500

    Update RunTrinotate.sh
    Added modified slurm version

--- Project_v5/final_assemblies/annotation/RunTrinotate.sh
+++ Project_v5/final_assemblies/annotation/RunTrinotate.sh
@@ -1,37 +1,42 @@
 #!/bin/bash 
 
 #SBATCH -J RunTrinotate                                 
-#SBATCH -p general
 #SBATCH -o Trinotate_%j.log
 #SBATCH -e Trinotate_%j.err 
 #SBATCH --mail-type=FAIL,BEGIN,END 
-#SBATCH --mail-user=
+#SBATCH --mail-user=yashsondhi@gmail.com
 #SBATCH --nodes=1 
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=5
 #SBATCH --mem=100gb
 #SBATCH --time=72:00:00 
+#SBATCH --cpus-per-task=16
 
 ##Move to correct WD
 source ../../setup_files/path_set
 
 cd $PWDHERE/final_assemblies/annotation
 
-export PATH=$PATH:$PWDHERE/software/Trinotate
-export PATH=$PATH:$PWDHERE/software/Blast
-export PATH=$PATH:$PWDHERE/software/SignalP
-export PATH=$PATH:$PWDHERE/software/TmHmm
-export PATH=$PATH:$PWDHERE/software/Hmmer
-export TRINOTATEDB="."
+export PATH=$PATH:$HPC_TRINOTATE_DIR/admin
+export PATH=$PATH:$HPC_TRINOTATE_DIR
 
+#export PATH=$PATH:$PWDHERE/software/Trinotate
+#export PATH=$PATH:$PWDHERE/software/Blast
+#export PATH=$PATH:$PWDHERE/software/SignalP
+#export PATH=$PATH:$PWDHERE/software/TmHmm
+#export PATH=$PATH:$PWDHERE/software/Hmmer
+#export TRINOTATEDB="."
+
+#Done previously
 makeblastdb -in uniprot_sprot.pep -dbtype prot
 
-Trinotate Trinotate.sqlite init --gene_trans_map genes_to_transcripts.tsv --transcript_fasta transcripts.main.fa --transdecoder_pep transcripts.reformated.aa
+#done previously
+Trinotate Trinotate.sqlite init --gene_trans_map genes_to_transcripts.tsv --transcript_fasta transcripts.main.fa --transdecoder_pep transcripts.main.transdecoder.pep.fa
 
-hmmscan --cpu 16 --domtblout PFAM.out $TRINOTATEDB/Pfam-A.hmm transcripts.reformated.aa > pfam.log &
+hmmscan --cpu 16 --domtblout PFAM.out Pfam-A.hmm transcripts.main.transdecoder.pep.fa > pfam.log &
 blastx -query transcripts.main.fa -db uniprot_sprot.pep -num_threads 16 -max_target_seqs 1 -outfmt 6 -evalue 1e-3 > blastx.out &
-blastp -query transcripts.reformated.aa -db uniprot_sprot.pep -num_threads 16 -max_target_seqs 1 -outfmt 6 -evalue 1e-3 > blastp.out &
-signalp -m transcripts.reformated.aa -n signalp.out -f short > signalp.out &
-tmhmm --short < transcripts.reformated.aa > tmhmm.out &
+blastp -query transcripts.main.transdecoder.pep.fa -db uniprot_sprot.pep -num_threads 16 -max_target_seqs 1 -outfmt 6 -evalue 1e-3 > blastp.out &
+signalp -m transcripts.main.transdecoder.pep.fa -n signalp.out -f short > signalp.out &
+tmhmm --short < transcripts.main.transdecoder.pep.fa > tmhmm.out &
 #RnammerTranscriptome.pl --transcriptome transcripts.main.fa --path_to_rnammer /usr/local/rnammer
 wait
 
@@ -43,3 +48,4 @@ Trinotate Trinotate.sqlite LOAD_swissprot_blastx blastx.out
 Trinotate Trinotate.sqlite LOAD_swissprot_blastp blastp.out
 
 Trinotate Trinotate.sqlite report > Report.xls
+
