4706423256c343d0c01426c3811e173bae7eba73
Yash Sondhi <yashsondhi@gmail.com>
2021-12-30 10:26:40 -0500

    Update RunTrinity.sh
    Add Scratch output directory
    Increased number of cores per task to 16 and added Trinity module for uf slurm cluster, modify accordingly to run on other machines

--- Project_v5/Trinity/RunTrinity.sh
+++ Project_v5/Trinity/RunTrinity.sh
@@ -1,14 +1,13 @@
 #!/bin/bash 
 
 #SBATCH -J RunTrinity                                   
-#SBATCH -p general
 #SBATCH -o Trinity_%j.log
 #SBATCH -e Trinity_%j.err 
 #SBATCH --mail-type=FAIL,BEGIN,END 
 #SBATCH --mail-user=
 #SBATCH --nodes=1 
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=220gb
 #SBATCH --time=4-0:0:00 
 
@@ -16,9 +15,11 @@ source ../setup_files/path_set
 cd $PWDHERE/Trinity
 
 export PATH=$PATH:$PWDHERE/software/Trinity
+module load trinity/2.12.0
+
 
 export reads=$PWDHERE/input_files/reads-norm.fq
 
-Trinity --max_memory 200G --seqType fq  --single $reads --CPU 6 --full_cleanup --output trinity_out_dir 
+Trinity --max_memory 200G --seqType fq  --single $reads --CPU 16 --full_cleanup --output $TMPDIR/trinity_out_dir 
 
-cp trinity_out_dir.Trinity.fasta ../final_assemblies/Trinity.fa
+cp $TMPDIR/trinity_out_dir.Trinity.fasta ../final_assemblies/Trinity.fa
